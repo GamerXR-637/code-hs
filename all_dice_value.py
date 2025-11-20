b768ab5578c6e26951d11042a242e9266522d8c8
Gamer-XR637 <168220319+GamerXR-637@users.noreply.github.com>
2025-11-20 18:13:29 -0500

    Create all_dice_value.py

---
+++ all_dice_value.py
@@ -0,0 +1,3 @@
+for o in range(1,7): # < loop 1 to get the first dice value
+    for t in range(1,7): # < loop 2 to get the seconud dice value
+        print(str(o) + "," + str(t)) # < print it as a pair and converts the integer to a string
