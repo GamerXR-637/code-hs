4667404bf5b1510a6cb0232959e610aeee883825
Gamer-XR637 <168220319+GamerXR-637@users.noreply.github.com>
2025-12-02 10:34:08 -0500

    Add height conversion function to meters
    This script converts height from feet and inches to meters.

---
+++ convert_height_to_meters.py
@@ -0,0 +1,16 @@
+INCHES_TO_CM = 2.54
+CM_TO_METERS = 0.01
+FEET_TO_INCHES = 12
+
+def convert_height_to_meters(feet, inches):
+    feet_cm = feet * FEET_TO_INCHES
+    inches_cm = feet_cm * INCHES_TO_CM + (inches * INCHES_TO_CM)
+    meter = inches_cm * CM_TO_METERS
+    print(meter)
+
+
+convert_height_to_meters(6, 4) # 1.9304 
+convert_height_to_meters(6, 5) # 1.9304 
+convert_height_to_meters(2, 8) # 1.9304 
+convert_height_to_meters(10, 6) # 1.9304 
+convert_height_to_meters(37, 5) # 1.9304 
